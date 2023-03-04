db3559f7779687fb784d3dcf3620de82001d2f74
Huy Minh <minh.th12558@sinhvien.hoasen.edu.vn>
2023-03-04 14:02:23 +0700

    update home page

--- TASK-TIME/src/app/pages/home/home.component.ts
+++ TASK-TIME/src/app/pages/home/home.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  projects: Array<any> = [
+    {
+      id: 1,
+      name: "Task Management",
+      description: "Create a task management website",
+      createDate: "28/02/2023",
+      deadline: "10/03/2023"
+    },
+    {
+      id: 2,
+      name: "UI Design",
+      description: "Design an UI for new website",
+      createDate: "20/02/2023",
+      deadline: "02/03/2023"
+    },
+    {
+      id: 3,
+      name: "Product Marketing",
+      description: "Marketing new products",
+      createDate: "20/02/2023",
+      deadline: "02/03/2023"
+    },
+    {
+      id: 4,
+      name: "Create Mobile App",
+      description: "Create a camera mobile app",
+      createDate: "14/02/2023",
+      deadline: "28/02/2023"
+    },
+    {
+      id: 5,
+      name: "Shopping Page",
+      description: "Create a shopping website",
+      createDate: "15/02/2023",
+      deadline: "02/03/2023"
+    },
+    {
+      id: 6,
+      name: "Update Game",
+      description: "Update FastFingers typing game",
+      createDate: "10/02/2023",
+      deadline: "25/02/2023"
+    },
+    {
+      id: 7,
+      name: "Create Ads",
+      description: "Create advertisements for company",
+      createDate: "10/02/2023",
+      deadline: "25/02/2023"
+    }
+  ];
 }
