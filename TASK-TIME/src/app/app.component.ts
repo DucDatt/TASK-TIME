dfd3ad3ea2dc50c6c154a36f30cd0a47b65b261d
khoa1406 <khoa.nd11530@sinhvien.hoasen.edu.vn>
2023-03-07 11:37:26 +0700

    update login/guard

--- TASK-TIME/src/app/app.component.ts
+++ TASK-TIME/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { onAuthStateChanged } from '@firebase/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TASK-TIME';
+  constructor(private auth:Auth,private route:Router){
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        console.log("user is logged in")
+        this.route.navigate(["home"])
+      } else {
+        console.log("user is not logged in")
+        this.route.navigate(["login"])
+      }
+    })
+  }
 }
--- TASK-TIME/src/app/pages/home/home.component.ts
+++ TASK-TIME/src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  
+export class HomeComponent implements OnInit {
+  constructor(private auth: AuthService){}
+
+user!: User | null;
+user$ = new Observable<User|null>();
+
+
+ ngOnInit(): void {
+     this.user$ = this.auth.user$;
+     this.user$.subscribe((user) => {
+       this.user = user;
+       console.log(user);
+     });
+ }
 }
