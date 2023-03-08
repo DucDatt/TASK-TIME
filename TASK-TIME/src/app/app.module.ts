151d3a51eb30893a8ced510353d65b1610e22b5a
Đạt Huỳnh <dat.hd3745@sinhvien.hoasen.edu.vn>
2023-03-08 23:57:50 +0700

    add, install ngrx

--- TASK-TIME/src/app/app.module.ts
+++ TASK-TIME/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MaterialModule } from 'src/shared/material.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
     MaterialModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
