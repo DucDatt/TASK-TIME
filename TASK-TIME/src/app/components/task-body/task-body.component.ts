d4afb978cc2936f77387f9e8b5308e623897d5fd
Đạt Huỳnh <dat.hd3745@sinhvien.hoasen.edu.vn>
2023-03-09 16:52:41 +0700

    .

--- TASK-TIME/src/app/components/task-body/task-body.component.ts
+++ TASK-TIME/src/app/components/task-body/task-body.component.ts
@@ -2,33 +2,33 @@ import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskPopupComponent } from '../task-popup/task-popup.component';
-import { animate, animation, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-task-body',
   templateUrl: './task-body.component.html',
   styleUrls: ['./task-body.component.scss'],
-    // animations: [
-    //   trigger('fadeInOut', [
-    //     transition(':enter', [
-    //       style({ opacity: 0 }),
-    //       animate('350ms', style({ opacity: 1 }))
-    //     ]),
-    //     transition(':leave', [
-    //       style({ opacity: 1 }),
-    //       animate('350ms', style({ opacity: 0 }))
-    //     ])
-    //   ]),
-    //   trigger('rotate', [
-    //     transition(':enter', [
-    //       animate('1000ms',keyframes([
-    //         style({transform: 'rotate(0deg)', offset: 0}),
-    //         style({transform: 'rotate(2turn)', offset: 1})
-    //       ])
-    //       )
-    //   ])
-    // ])
-    // ]
+    animations: [
+      trigger('fadeInOut', [
+        transition(':enter', [
+          style({ opacity: 0 }),
+          animate('350ms', style({ opacity: 1 }))
+        ]),
+        transition(':leave', [
+          style({ opacity: 1 }),
+          animate('350ms', style({ opacity: 0 }))
+        ])
+      ]),
+      trigger('rotate', [
+        transition(':enter', [
+          animate('1000ms',keyframes([
+            style({transform: 'rotate(0deg)', offset: 0}),
+            style({transform: 'rotate(2turn)', offset: 1})
+          ])
+          )
+      ])
+    ])
+    ]
 })
 export class TaskBodyComponent {
   constructor(private dialog: MatDialog) { }
